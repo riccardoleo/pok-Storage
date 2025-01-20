25dcf2af375db578e3e9001fe71f92db4ad1cb91
riccardoleo <leoriccardo24@gmail.com>
2025-01-20 16:21:03 +0100

    aggiunte API nel controller

--- pok-Storage/src/main/java/org/model/Card.java
+++ pok-Storage/src/main/java/org/model/Card.java
@@ -5,8 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.CollectionCallback;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "album")
 @Data
@@ -16,9 +18,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Card {
 
     @Id
-    private String id;
+    private int id;
     private String name;
     private String generation;
     private String cardCode;
 
+    public List findAll(){
+        List list = new ArrayList();
+        list.add(this);
+        return list;
+    }
 }
